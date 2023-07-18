d241f1a8a00803106bdd3bdc846fdc9b5ef0a427
Fabian Emmerich <fabian.emmerich@4-cast.de>
2023-07-18 12:17:46 +0200

    Add tests and workflow step

---
+++ tests/unit/plotting/deepclusterv2/test_embedding.py
@@ -0,0 +1,37 @@
+import torch
+import vissl.plotting.deepclusterv2.embeddings as _embeddings
+
+
+def test_plot_embedding(tmp_path):
+    # Embeddings for 2 crops, 2 samples, and embedding dims of size 4
+    embeddings = torch.Tensor(
+        [
+            [
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+            ],
+            [
+                [1, 2, 3, 4],
+                [5, 6, 7, 8],
+            ],
+        ]
+    )
+
+    # Assignments for 2 samples
+    assignments = torch.Tensor(
+        [1, 2],
+    )
+
+    expected = [
+        tmp_path / "test-crops-0.pdf",
+        tmp_path / "test-crops-1.pdf",
+    ]
+
+    _embeddings.plot_embeddings_using_tsne(
+        embeddings=embeddings,
+        assignments=assignments,
+        name="test",
+        output_dir=tmp_path.as_posix(),
+    )
+
+    assert all(plot.exists() for plot in expected)
